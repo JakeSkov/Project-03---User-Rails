ab5fe4826dac8f1d316a9056ebebaaf7aa68de2f
LivingValkyrie <deadwynn@gmail.com>
2015-09-24 09:41:41 -0500

    added window and actual export. still buggy
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ RailEnginev2/Assets/Editor/ExportWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: ExportWindow 
+/// </summary>
+public class ExportWindow : EditorWindow {
+    #region Fields
+
+    #endregion
+
+    void OnGUI() {
+        EditorGUILayout.LabelField("Level Information");
+        string name = EditorGUILayout.TextField("enter Level name");
+        string author = EditorGUILayout.TextField( "enter author name" );
+        if (GUILayout.Button("Export")) {
+            ExportWaypoints.Export(author, name);
+        }
+    }
+
+}
