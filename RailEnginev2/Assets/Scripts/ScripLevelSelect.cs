fcee3568903a2a055fbb13718fa6e9c06bd090fc
JakeSkov <JakeSkov95@gmail.com>
2015-09-26 09:01:58 -0500

    updates

---
+++ RailEnginev2/Assets/Scripts/ScripLevelSelect.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class ScripLevelSelect : MonoBehaviour 
+{
+    private Canvas[] levelPages;
+    private Button[] buttons;
+    private TextAsset[] levelText;
+
+    public static Button[] levelButton = new Button[8];
+
+    public Button nextPage;
+    public Button prevPage;
+
+	void Awake () 
+    {
+        levelPages = FindObjectsOfType<Canvas>();
+        buttons = FindObjectsOfType<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i].tag == "LevelButton" && i < levelButton.GetLength(0))
+            {
+                levelButton[i] = buttons[i];
+                Debug.Log(levelButton.Length);
+            }
+        }
+
+        if (levelPages.Length > 1)
+        {
+            Debug.Log("Wat?");
+        }
+        else
+        {
+            prevPage.interactable = false;
+            nextPage.interactable = false;
+        }
+
+        for (int i = 0; i > levelButton.Length; i++)
+        {
+            if (levelButton[i] == null)
+            {
+                levelButton[i].interactable = false;
+            }
+        }
+	}
+}
---
+++ RailEnginev2/Assets/Scripts/ScriptLevelButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class ScriptLevelButton : MonoBehaviour 
+{
+    public Text levelName;
+    public Text authorName;
+    public Text dateText;
+    public Image levelImage;
+
+    TextAsset[] levelFile;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StreamReader sr;
+        levelFile = FindObjectsOfType<TextAsset>();
+        int lineNum = 0;
+
+        for (int i = 0; i < ScripLevelSelect.levelButton.Length; i++)
+        {
+            sr = new StreamReader("/" + levelFile[i]);
+
+            string levelInfo = sr.ReadLine();
+            lineNum++;
+
+            if (lineNum == 1)
+            {
+                levelName.text = levelInfo;
+            }
+            if (lineNum == 2)
+            {
+                dateText.text = levelInfo;
+            }
+            if (lineNum == 3)
+            {
+                authorName.text = levelInfo;
+            }
+        }
+	}
+    
+}
